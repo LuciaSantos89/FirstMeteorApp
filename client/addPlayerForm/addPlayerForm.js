d1dc122e46436c757651ea5fff12e309ca522e3b
lucia <analucia0189@gmail.com>
2015-10-22 16:57:48 -0600

    structure files

---
+++ client/addPlayerForm/addPlayerForm.js
@@ -0,0 +1,7 @@
+Template.addPlayerForm.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var playerNameVar = event.target.playerName.value;
+        Meteor.call('insertPlayerData', playerNameVar);
+    }
+});
---
+++ client/leaderBoardList/leaderbordList.js
@@ -0,0 +1,41 @@
+Template.leaderboard.helpers({
+    'player': function() {
+        var currentUserId = Meteor.userId();
+        return PlayersList.find({}, {
+            sort: {
+                score: -1,
+                name: 1
+            }
+        });
+    },
+    'selectedClass': function() {
+        var playerId = this._id;
+        var selectedPlayer = Session.get('selectedPlayer');
+        if (playerId == selectedPlayer) {
+            return "selected"
+        }
+    },
+    'showSelectedPlayer': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+
+        return PlayersList.findOne(selectedPlayer)
+    }
+});
+Template.leaderboard.events({
+    'click .player': function() {
+        var playerId = this._id;
+        Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        Meteor.call('modifyPlayerScore', selectedPlayer, 5);
+    },
+    'click .decrement': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        Meteor.call('modifyPlayerScore', selectedPlayer, -5);
+    },
+    'click .remove': function() {
+        var selectedPlayer = Session.get('selectedPlayer');
+        Meteor.call('removePlayerData', selectedPlayer);
+    }
+});
